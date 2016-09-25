32b4ec7be93bb5542f046d7448c37b0045095603
magnumdoomguy <lawrence.popa@mohawkcollege.ca>
2016-09-25 10:32:51 -0400

    Prescale Music Genre Icons Script

---
+++ prescalemusicgenres.sh
@@ -0,0 +1,4 @@
+rm ~/.plexht/addons/skin.aeon.nox.5/media/genres/music/105x105/*.*
+rm ~/.plexht/addons/skin.aeon.nox.5/media/genres/music/335x335/*.*
+mogrify -path ~/.plexht/addons/skin.aeon.nox.5/media/genres/music/105x105/ -filter LanczosSharp -distort Resize 105x105\! -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB ~/skin.aeon.nox.5.images.master/genres/Music/*.*
+mogrify -path ~/.plexht/addons/skin.aeon.nox.5/media/genres/music/335x335/ -filter LanczosSharp -distort Resize 335x335\! -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB ~/skin.aeon.nox.5.images.master/genres/Music/*.*
