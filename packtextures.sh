dbb9f883c3034e643fbffa4d64bab9bd5f06e9a8
magnumdoomguy <lawrence.popa@mohawkcollege.ca>
2017-01-21 10:31:01 -0500

    Media flags fixes

---
+++ packtextures.sh
@@ -0,0 +1,2 @@
+rm ~/.plexht/addons/skin.aeon.nox.5/media/textures.xbt
+TexturePacker -dupecheck -input ~/.plexht/addons/skin.aeon.nox.5/media/ -output ~/.plexht/addons/skin.aeon.nox.5/media/textures.xbt
